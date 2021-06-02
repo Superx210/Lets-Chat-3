b2bcae99df140105b68937409a37966fe3d2811b
Superx210 <77707693+Superx210@users.noreply.github.com>
2021-06-02 08:20:36 -0700

    Add files via upload

---
+++ Lets chat (2).js
@@ -0,0 +1,36 @@
+//ADD YOUR FIREBASE LINKS HERE
+function addRoom() {
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child("room_name").update({
+
+    });
+
+    localStorage.setItem("room-name", room_name);
+
+    window.location = "kwitter_page.html"
+}
+
+function getData() {
+    firebase.database().ref("/").on('value', function(snapshot) {
+        document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function(childSnapshot) {
+            childKey = childSnapshot.key;
+            Room_names = childKey;
+            //Start code
+            console.log("Room Name-" + Room_names);
+            row = "<div class='room-name'id=" + Room_names + " onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hrs>"
+            document.getElementById("output").innerHTML += row;
+
+            //End code
+        });
+    });
+
+}
+getData();
+
+function redirectToRoomName(name) {
+    console.log(name);
+    localStorage.setItem("room_name".name);
+    window.location = "kwitter_page.html";
+}
